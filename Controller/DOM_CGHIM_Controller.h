41cdaa02669a6981d183f18ab65dd1625f4323c5
Henrique <hahollanda@gmail.com>
2025-10-06 23:33:13 -0300

    Controller.h

---
+++ Controller/DOM_CGHIM_Controller.h
@@ -0,0 +1,8 @@
+
+void fInicializaPecas(tipopeca p[28]);
+void fEmbaralhar(tipopeca p[28]);
+void fImprimeEmb(tipopeca p[28]);
+void fMenu(tipopeca p[28]);
+int fDistribuirPecas(int n);
+int fQmComc();
+int fEscolhaPeca(int pescolha, int ladoO, int ladoE, int x);
