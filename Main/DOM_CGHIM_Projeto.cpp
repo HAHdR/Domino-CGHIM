70c0d6f3c41aa98a353472ee80149f5af5d9140b
Henrique <hahollanda@gmail.com>
2025-10-06 23:45:53 -0300

    DOM_CGHIM_Projeto.cpp

---
+++ Main/DOM_CGHIM_Projeto.cpp
@@ -0,0 +1,17 @@
+/*
+DOM-CGHIM-Projeto.cpp - Projeto Domino - Etapa 2
+22/08/2024 - Grupo: CGHIM
+Carolina Olmos Gracio Castanho
+Geovanna Cristina Brezinger
+Henrique Abreu Hollanda da Rocha
+Isabela Nunes dos Santos
+Matheus Guido Noronha Moreira Passos
+*/
+
+#include "Dom_CGHIM_Model.cpp"
+#include "Dom_CGHIM_Controller.cpp"
+
+main(){
+	tipopeca pecas[28];
+	fMenu(pecas);
+}
