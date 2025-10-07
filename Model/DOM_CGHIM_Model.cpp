32a232d1efe8ba50e8d8037da3fedf5d048a2b55
Henrique <hahollanda@gmail.com>
2025-10-06 23:37:18 -0300

    Model.cpp

---
+++ Model/DOM_CGHIM_Model.cpp
@@ -0,0 +1,18 @@
+/*
+DOM-CGHIM-Model.cpp - Projeto Domino - Etapa 2
+14/08/2024 - Grupo: CGHIM
+Carolina Olmos Gracio Castanho
+Geovanna Cristina Brezinger 
+Henrique Abreu Hollanda da Rocha
+Isabela Nunes dos Santos
+Matheus Guido Noronha Moreira Passos
+*/
+
+#include "Dom_CGHIM_Model.h"
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+    int lado1;
+    int lado2;
+} tipopeca;
